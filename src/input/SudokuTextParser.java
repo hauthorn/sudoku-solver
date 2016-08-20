b3873f262477de7e0ac66f7317fe72729bbc2f6c
Christoffer Hauthorn <christoffer@emplate.it>
2016-08-21 01:50:50 +0200

    Minor changes to SudokuTextParser

--- src/input/SudokuTextParser.java
+++ src/input/SudokuTextParser.java
@@ -5,7 +5,7 @@ import model.Board;
 import java.io.*;
 
 /**
- * Created by CHauthorn on 20-08-2016.
+ * Parses the Sudoku file
  */
 public class SudokuTextParser {
     public static Board parseBoardFromFile(String boardName) {
@@ -16,17 +16,15 @@ public class SudokuTextParser {
             while ((line = br.readLine()) != null) {
                 if (line.equals(boardName)) {
                     // Parse the next lines as a board
-                    System.out.println("Parsing: " + line);
                     Board board = new Board();
 
                     // Run through the next 9 lines
                     for (int i = 0; i < 9; i++) {
                         String gridLine = br.readLine();
-                        System.out.println(gridLine);
                         for (int j = 0; j < 9; j++) {
                             String symbol = gridLine.substring(j, j + 1);
                             int value = Integer.parseInt(symbol);
-                            board.setField(i, j, value);
+                            board.setField(j, i, value);
                         }
                     }
                     return board;
